bcb892380054c2489873052450af41c6488d7c0d
Jason Choi <1027387561@qq.com>
2020-06-12 00:05:36 +0800

    remove duplicates from sorted array (leetcode 26)

---
+++ js/remove-duplicates.js
@@ -0,0 +1,16 @@
+var removeDuplicates = function(nums) {
+  let len = nums.length
+  if (len === 0) {
+    return 0
+  }
+  let i = 0
+  for (let j = 1; j < len; j++) {
+    if (nums[j] !== nums[i]) {
+      i++
+      nums[i] = nums[j]
+    }
+  }
+  return i + 1
+};
+
+console.log(removeDuplicates([1, 1, 2, 3, 3]))
